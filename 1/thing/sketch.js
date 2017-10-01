ff3745d955330f3cd3c66eafe2ed46a1631045aa
Becca Rose <hello@beccarose.co.uk>
2017-10-01 09:55:42 +0100

    tweeks

--- 1/thing/sketch.js
+++ 1/thing/sketch.js
@@ -1,70 +1,61 @@
-//Changing the sprites' animations
-//position and transformations: rotation, scale, mirror
-//move the mouse and click
-//press and hold the up and down keys
+//inktober 2017
+//1 
+//first a thing
+//this p5 play library is amazing thankyou.
 
-var ghost;
+var thing; // this is the sprite object
 
 function setup() {
-  createCanvas(800,300);
-  
-  //create a sprite and add the 3 animations
-  ghost = createSprite(400, 150, 50, 100);
-  
-  //label, first frame, last frame
-  //the addAnimation method returns the added animation
-  //that can be store in a temporary variable to change parameters
-  var myAnimation = ghost.addAnimation("floating", "assets/ghost_standing0001.png", "assets/ghost_standing0007.png");
-  //offX and offY is the distance of animation from the center of the sprite
-  //in this case since the animations have different heights i want to adjust
-  //the vertical offset to make the transition between floating and moving look better
-  myAnimation.offY = 18;
-  
-  ghost.addAnimation("moving", "assets/ghost_walk0001.png", "assets/ghost_walk0004.png");
-  
-  ghost.addAnimation("spinning", "assets/ghost_spin0001.png", "assets/ghost_spin0003.png");
-  
+
+  createCanvas(windowWidth, windowHeight);
+  //create a  new sprite object 
+  thing = createSprite(200, 150, 150, 200);
+
+  //add the 3 animations to the object
+  //(label, first frame, last frame)
+  thing.addAnimation("floating", "thing_still01.png", "thing_still02.png");
+  thing.addAnimation("moving", "thing_walk01.png", "thing_walk02.png");
+  thing.addAnimation("spinning", "thing_walk01.png", "thing_walk01.png");
+
 }
 
 function draw() {
-  background(255,255,255);  
-  
-  //if mouse is to the left
-  if(mouseX < ghost.position.x - 10) {
-    ghost.changeAnimation("moving");
+  background(255, 255, 255);
+
+  //move LEFT towards mouse
+  if (mouseX < thing.position.x + 10) {
+    thing.changeAnimation("moving");
     //flip horizontally
-    ghost.mirrorX(-1);
+    thing.mirrorX(1);
     //negative x velocity: move left
-    ghost.velocity.x = - 2;
+    thing.velocity.x = -1;
   }
-  else if(mouseX > ghost.position.x + 10) {
-    ghost.changeAnimation("moving");
+
+  //move RIGHT towards mouse
+  else if (mouseX > thing.position.x - 10) {
+    thing.changeAnimation("moving");
     //unflip 
-    ghost.mirrorX(1);
-    ghost.velocity.x = 2;
+    thing.mirrorX(-1);
+    thing.velocity.x = 1;
   }
+        //STILL
   else {
-    //if close to the mouse, don't move
-    ghost.changeAnimation("floating");
-    ghost.velocity.x = 0;
+    //if close to the mouse
+    thing.changeAnimation("floating"); //change to floating animation sequance
+    thing.velocity.x = 0; //don't move
   }
-  
-  if(mouseIsPressed) {
+
+  // SPINNING
+  if (mouseIsPressed) {
     //the rotation is not part of the spinning animation
-    ghost.rotation -= 10;
-    ghost.changeAnimation("spinning");
+    thing.rotation -= 10;
+    thing.changeAnimation("spinning");
   }
   else
-    ghost.rotation = 0;
-  
-  //up and down keys to change the scale
-  //note that scaling the image quality deteriorates
-  //and scaling to a negative value flips the image
-  if(keyIsDown(UP_ARROW))
-    ghost.scale += 0.05;
-  if(keyIsDown(DOWN_ARROW))
-    ghost.scale -= 0.05;
-  
+    thing.rotation = 0;
+    
+
+
   //draw the sprite
   drawSprites();
 }
